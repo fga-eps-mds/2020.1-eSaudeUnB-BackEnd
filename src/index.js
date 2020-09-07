619e9f38700b04241a484eecad3123506d0a185b
Daniel Goncalves <danmke@hotmail.com>
2020-09-07 16:47:30 -0300

    chore: configure husky
    Co-authored-by: joberth.rogers18 <joberth.rogers18@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-/**
- * Required External Modules
- */
-
-const express = require("express");
-const path = require("path");
-
-/**
- * App Variables
- */
+const express = require('express');
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || '8000';
 
-/**
- * Routes Definitions
- */
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
-/**
- * Server Activation
- */
-
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-});
+app.listen(port);
