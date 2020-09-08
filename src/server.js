870c42c275460c0e498cde9fece98c906f866ca4
Daniel Goncalves <danmke@hotmail.com>
2020-09-08 00:40:29 -0300

    feat: add structure of endpoint tests
    Signed-off-by: Daniel Goncalves <danmke@hotmail.com>

--- src/server.js
+++ src/server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
-const port = process.env.PORT || '8000';
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port);
+module.exports = app;
