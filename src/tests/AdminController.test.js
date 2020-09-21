30552b1a3c36616b4b469d822ec7024482d06d17
Vinicius <viniciusfa.delima@gmail.com>
2020-09-21 16:49:00 -0300

    feat: add error test cases

--- src/tests/AdminController.test.js
+++ src/tests/AdminController.test.js
@@ -30,13 +30,25 @@ describe('Admin API', () => {
         const response = await request.post('/admin').send(user);
 
         expect(response.status).toBe(201);
+
+        const respose2 = await request.post('/admin').send(user);
+        expect(respose2.status).toBe(200);
     });
 
-    it('should be able to login an admin', async() => {
+    it('should be able to login an admin', async () => {
         await request.post('/admin').send(user);
-        const {email, password} = user;
-        const response = await request.post('/admin/login').send({email, password});
+        const { email, password } = user;
+        const response = await request.post('/admin/login').send({ email, password });
 
         expect(response.status).toBe(200);
+
+        const email2 = 'testemail@test.com'
+        const password2 = 'password'
+
+        const response2 = await request.post('/admin/login').send({email2, password2});
+        expect(response2.status).toBe(404)
+
+        const response3 = await request.post('/admin/login').send({email, password2});
+        expect(response3.status).toBe(400);
     });
 });
