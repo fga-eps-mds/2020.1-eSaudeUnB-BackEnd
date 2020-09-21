1d4ba225f534e87e5ec15e9f92fad665161cdcb4
unknown <abner.f.c.r@hotmail.com>
2020-09-20 23:09:26 -0300

    refactor: lint changes

--- src/controllers/PatientController.js
+++ src/controllers/PatientController.js
@@ -1,13 +1,11 @@
-const uuid = require('uuid');
 const UserPatient = require('../models/UserPatient');
 
 module.exports = {
 
     async store(req, res) {
         try {
-
             const {
-                name, lastName, email, password
+                name, lastName, email, password,
             } = req.body;
 
             await UserPatient.create({
@@ -58,66 +56,66 @@ module.exports = {
     },
 
     async update(req, res) {
-        try{
-        const {
-            name, lastName, email, phone, unbRegistration, gender, bond, civilStatus, religion
-        } = req.body;
+        try {
+            const {
+                name, lastName, email, phone, unbRegistration, gender, bond, civilStatus, religion,
+            } = req.body;
 
-        const user = await UserPatient.findOne({
-           email: req.params.email
-        }).exec();
+            const user = await UserPatient.findOne({
+                email: req.params.email,
+            }).exec();
 
-        if(name){
-           user.name = name; 
-        }
-        if(lastName){
-            user.lastName = lastName; 
-         }
-         if(email){
-            user.email = email; 
-         }
-         if(phone){
-            user.phone = phone; 
-         }
-         if(unbRegistration){
-            user.unbRegistration = unbRegistration; 
-         }
-         if(gender){
-            user.gender = gender; 
-         }
-         if(bond){
-            user.bond = bond; 
-         }
-         if(civilStatus){
-            user.civilStatus = civilStatus; 
-         }
-         if(religion){
-            user.religion = religion; 
-         }
-         await user.save();
-        return res.status(200).json(user);
-        }catch(err){
-            return res.status(500).json({message: "falha ao dar o update"})
+            if (name) {
+                user.name = name;
+            }
+            if (lastName) {
+                user.lastName = lastName;
+            }
+            if (email) {
+                user.email = email;
+            }
+            if (phone) {
+                user.phone = phone;
+            }
+            if (unbRegistration) {
+                user.unbRegistration = unbRegistration;
+            }
+            if (gender) {
+                user.gender = gender;
+            }
+            if (bond) {
+                user.bond = bond;
+            }
+            if (civilStatus) {
+                user.civilStatus = civilStatus;
+            }
+            if (religion) {
+                user.religion = religion;
+            }
+            await user.save();
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(500).json({ message: 'falha ao dar o update' });
         }
     },
 
-    async updatePassword(req,res) {
-        try{
-        const{
-            password
-        } = req.body
+    async updatePassword(req, res) {
+        try {
+            const {
+                password,
+            } = req.body;
 
-        const user = await UserPatient.findOne({
-            email: req.params.email
-         }).exec();
+            const user = await UserPatient.findOne({
+                email: req.params.email,
+            }).exec();
 
-        user.password = password
+            user.password = password;
 
-        await user.save();
-        return res.status(200).json(user);
-        }catch(err){
-            return res.status(500).json({message: "falha ao dar o update da senha"})
+            await user.save();
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(500).json({ message: 'falha ao dar o update da senha' });
         }
-    }
+    },
 
 };
