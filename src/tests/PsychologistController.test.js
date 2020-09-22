815cf7837fac2842edc58179b4b47f7725f23c61
Rafaelltm <rafaelltm10@hotmail.com>
2020-09-22 18:09:58 -0300

    refactor: increased tests coverage
    Co-authored-by: abner423 <abner.f.c.r@hotmail.com>

--- src/tests/PsychologistController.test.js
+++ src/tests/PsychologistController.test.js
@@ -25,6 +25,16 @@ const user2 = {
     bibliography: '',
 };
 
+const user3 = {
+    name: 'teste',
+    lastName: 'abner',
+    email: null,
+    gender: 'M',
+    bond: 'graduando',
+    specialization: '',
+    bibliography: '',
+};
+
 describe('Psychologist API', () => {
     beforeAll(async () => {
         mongoose.connect(process.env.MONGO_URL, {
@@ -40,17 +50,23 @@ describe('Psychologist API', () => {
         done();
     });
 
+    it('should be able to create a new psychologist', async () => {
+        const response = await request.post('/admin/psy/create').send(user1);
+
+        expect(response.status).toBe(201);
+    });
+
+    it('should not be able to create a new psychologist', async () => {
+        const response = await request.post('/admin/psy/create').send(user3);
+
+        expect(response.status).toBe(400);
+    });
+
     it('should be able to list all the psychologists', async () => {
         await request.post('/admin/psy/create').send(user1);
         await request.post('/admin/psy/create').send(user2);
 
         const response = await request.get('/admin/psy/list');
-
-        expect(response.status).toBe(200);
-    });
-
-    it('should be able to create a new psychologist', async () => {
-        const response = await request.post('/admin/psy/create').send(user1);
 
         expect(response.status).toBe(200);
     });
@@ -66,9 +82,33 @@ describe('Psychologist API', () => {
     it('should be able to update a psychologist', async () => {
         await request.post('/admin/psy/create').send(user1);
 
-        const responseDelete = await request.put(`/psyUpdate/${user1.email}`).send({ email: 'teste@hotmail.com' });
+        const responseDelete = await request.put(`/psyUpdate/${user1.email}`).send({
+            name: 'teste',
+            lastName: 'abner',
+            email: 'abcdefghij@hotmail.com',
+            gender: 'M',
+            bond: 'graduando',
+            specialization: 'Formado na UnB',
+            bibliography: '2020200',
+        });
 
         expect(responseDelete.status).toBe(200);
+    });
+
+    it('should not be able to update a psychologist', async () => {
+        await request.post('/admin/psy/create').send(user1);
+
+        const responseDelete = await request.put(`/psyUpdate/${null}`).send({
+            name: 'teste',
+            lastName: 'abner',
+            email: 'abcdefghij@hotmail.com',
+            gender: 'M',
+            bond: 'graduando',
+            specialization: 'Formado na UnB',
+            bibliography: '2020200',
+        });
+
+        expect(responseDelete.status).toBe(500);
     });
 
     it('should be able to update a psychologist password', async () => {
@@ -77,5 +117,13 @@ describe('Psychologist API', () => {
         const responseDelete = await request.put(`/psyUpdatePassword/${user1.email}`).send({ password: 123 });
 
         expect(responseDelete.status).toBe(200);
+    });
+
+    it('should not be able to update a psychologist password', async () => {
+        await request.post('/admin/psy/create').send(user1);
+
+        const responseDelete = await request.put(`/psyUpdatePassword/${user1.email}`).send({ password: null });
+
+        expect(responseDelete.status).toBe(500);
     });
 });
