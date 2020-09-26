d0906c05029944929811ce857f3e8ca6128f80f2
Daniel Maike <danmke@hotmail.com>
2020-09-26 19:54:33 -0300

    feat: add mongo by env variable
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://root:esaude@mongo:27017', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
