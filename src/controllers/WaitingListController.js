fcaca06c0d08413b99d207efd00a1125357ed7c7
unknown <abner.f.c.r@hotmail.com>
2020-11-09 01:02:43 -0300

    feat: add waitingList structure
    Co-authored-by: Rafaelltm <rafaelltm10@hotmail.com>

--- src/controllers/WaitingListController.js
+++ src/controllers/WaitingListController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const WaitingList = require('../models/WaitingList');
 
 module.exports = {
@@ -8,6 +9,20 @@ module.exports = {
                 emailPatient,
                 namePatient,
             } = req.body;
+=======
+const { update } = require('../models/WaitingList');
+const WaitingList = require('../models/WaitingList');
+const { show } = require('./PatientController');
+
+module.exports = {
+    async store(req,res){
+        try{
+            const{
+                email,
+                emailPatient,
+                namePatient
+            } = req.body
+>>>>>>> 1c5e3fa... feat: add waitingList structure
 
             const waitingList = await WaitingList.create({
                 email,
@@ -16,11 +31,17 @@ module.exports = {
             });
 
             return res.status(201).json(waitingList);
+<<<<<<< HEAD
         } catch (err) {
+=======
+
+        }catch(err){
+>>>>>>> 1c5e3fa... feat: add waitingList structure
             return res.status(400).json({ error: err.message });
         }
     },
 
+<<<<<<< HEAD
     async index(req, res) {
         try {
             const { email } = req.params;
@@ -31,19 +52,60 @@ module.exports = {
 
             return res.status(200).json(waitingLists);
         } catch (err) {
+=======
+    async index(req,res){
+        try{
+            const email = req.params
+
+            const waitingLists = await WaitingList.find({
+                email: email
+            }).exec()
+
+            return res.status(200).json(waitingLists);
+        }catch(err){
+>>>>>>> 1c5e3fa... feat: add waitingList structure
             return res.status(400).json({ error: err.message });
         }
     },
 
+<<<<<<< HEAD
     async destroy(req, res) {
         try {
             const { email } = req.params;
 
             await WaitingList.deleteOne({ emailPatient: email });
+=======
+    async destroy(req,res){
+        try {
+            const { email } = req.params;
+
+            await WaitingList.deleteOne({ email });
+>>>>>>> 1c5e3fa... feat: add waitingList structure
 
             return res.status(200).json();
         } catch (err) {
             return res.status(400).json({ message: err.message });
         }
     },
+<<<<<<< HEAD
 };
+=======
+
+    async update(req,res){
+        try{
+            const {email, patients} = req.body
+
+            const waitingList = await WaitingList.findOne({
+                email,
+            }).exec();
+
+            waitingList.patients = patients
+
+            await waitingList.save();
+            return res.status(200).json(waitingList);
+        }catch(err){
+            return res.status(500).json({ message: 'falha ao dar o update' });
+        }
+    }
+}
+>>>>>>> 1c5e3fa... feat: add waitingList structure
--- src/models/WaitingList.js
+++ src/models/WaitingList.js
@@ -3,13 +3,26 @@ const mongoose = require('mongoose');
 const WaitingListSchema = new mongoose.Schema({
     email: {
         type: String,
+<<<<<<< HEAD
+=======
+        unique:true,
+>>>>>>> 1c5e3fa... feat: add waitingList structure
         required: true,
     },
     namePatient: String,
     emailPatient: {
         type: String,
+<<<<<<< HEAD
         required: true,
     },
 }, { timestamps: true });
 
 module.exports = mongoose.model('WaitingList', WaitingListSchema);
+=======
+        unique:true,
+        required: true,
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model('WaitingList', WaitingListSchema);
+>>>>>>> 1c5e3fa... feat: add waitingList structure
