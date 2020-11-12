d903e45fb6d452b147342329ed438a6810377574
unknown <abner.f.c.r@hotmail.com>
2020-11-12 17:06:28 -0300

    fix: making lint changes at model waitingList

--- src/models/WaitingList.js
+++ src/models/WaitingList.js
@@ -22,7 +22,8 @@ module.exports = mongoose.model('WaitingList', WaitingListSchema);
         unique:true,
         required: true,
     },
-}, { timestamps: true })
+}, { timestamps: true },
+);
 
 module.exports = mongoose.model('WaitingList', WaitingListSchema);
 >>>>>>> 1c5e3fa... feat: add waitingList structure
