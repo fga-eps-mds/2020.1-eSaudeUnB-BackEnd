9f4f0af8fefb516642f363e392bf8696edf92fe7
Luis Marques <luisgustavomarques@outlook.com>
2020-12-12 08:14:30 -0300

    fix:change email.config formate

--- src/config/email.config.js
+++ src/config/email.config.js
@@ -1,15 +1,12 @@
 const nodemailer = require('nodemailer');
 
-module.exports = {
-    async transporter() {
-        return (nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user: 'esaudtest@gmail.com',
-                pass: 'uR64JhObSqU0',
-            },
-        }));
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'esaudtest@gmail.com',
+        pass: 'uR64JhObSqU0',
     },
-};
+});
+module.exports = transporter;
